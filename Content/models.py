4251be17ec42069635e21b2d0b8b1b0630f863e3
Supamid44m <supamid780@gmail.com>
2022-12-01 01:17:02 +0700

    change

--- Content/models.py
+++ Content/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title=models.CharField(max_length=500)
+    desciption=models.TextField(max_length=500)
+    
+
+
+
+
--- Content/views.py
+++ Content/views.py
@@ -1,3 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from Content.models import *
 
 # Create your views here.
+def showNews(req):
+    context={"News":News.objects.all()}
+    return render(req,'Content/header.html',context={})
